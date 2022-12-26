538b6ea775be72a998f2d539e81ab714f588e77e
mamadidiarrisso <diarrisso49@gmail.com>
2022-12-26 22:19:45 +0100

    ajout de nouvelle propriete

---
+++ src/Model/Mitarbeiter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Model;
+
+class Mitarbeiter
+{
+    private $vorname;
+    private $nachname;
+    private $adresse;
+    private $ort;
+    private $plz;
+    private $eintritdatum;
+    private $austritdatum;
+    private $fonction;
+    private $numeremployer;
+    private $created;
+    private $deleted;
+
+}
